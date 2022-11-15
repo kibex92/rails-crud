a859518436ee1dc8a37b8b51b48b010ec9dfb05a
Boris Knezevic <boris.knezevic0@gmail.com>
2022-11-15 11:15:27 +0100

    add edit + destroy

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     # Find all restaurants -> Restaurant.all
     # Store in an instance variable
@@ -7,7 +7,6 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -20,9 +19,29 @@ class RestaurantsController < ApplicationController
     redirect_to restaurants_path
   end
 
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    # Find the restaurant -> Restaurant.find
+    # Destroy
+    @restaurant.destroy
+    # redirect to the index
+    redirect_to restaurants_path, status: :see_other
+  end
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # VERB 'path', to: 'controller#action', as: ':prefix'
-  get "restaurants", to: 'restaurants#index', as: :restaurants
-  get "restaurants/new", to: 'restaurants#new', as: :new_restaurant
-  get "restaurants/:id", to: 'restaurants#show', as: :restaurant
-  post "restaurants", to: 'restaurants#create'
+  # get "restaurants", to: 'restaurants#index', as: :restaurants
+  # get "restaurants/new", to: 'restaurants#new', as: :new_restaurant
+  # get "restaurants/:id", to: 'restaurants#show', as: :restaurant
+  # post "restaurants", to: 'restaurants#create'
+  # get "restaurants/:id/edit", to: 'restaurants#edit', as: :edit_restaurant
+  # patch "restaurants/:id", to: 'restaurants#update'
+  # delete "restaurants/:id", to: 'restaurants#destroy'
+  resources :restaurants
 end
