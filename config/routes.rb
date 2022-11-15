6cc36449c0022d0a697d339965f3c219711a8ed4
Boris Knezevic <boris.knezevic0@gmail.com>
2022-11-15 10:37:50 +0100

    Add create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # VERB 'path', to: 'controller#action', as: ':prefix'
   get "restaurants", to: 'restaurants#index', as: :restaurants
+  get "restaurants/new", to: 'restaurants#new', as: :new_restaurant
   get "restaurants/:id", to: 'restaurants#show', as: :restaurant
+  post "restaurants", to: 'restaurants#create'
 end
